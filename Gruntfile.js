295e7bd59e6850f801bf685b021dd970c0ff7d2a
Andrey Arkhipov <wibox@ya.ru>
2014-01-13 11:42:22 +0400

    Settings optimize images for grunt.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,15 +58,29 @@ module.exports = function (grunt) {
                 }
             }
         },
+        imagemin: {                          // Task
+            dynamic: {                         // Another target
+                files: [{
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: 'assets/img/',                   // Src matches are relative to this path
+                    src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'public_html/img/'                  // Destination path prefix
+                }]
+            }
+        },
 
         watch: {
             scripts: {
                 files: ['assets/js/*.js'],
-                tasks: ['concat', 'uglify', 'removelogging']
+                tasks: ['concat', 'uglify', 'removelogging','jshint']
             },
             css: {
                 files: ['assets/less/*.less'],
                 tasks: ['less', 'cssmin']
+            },
+            image: {
+                files: ['assets/img/*.*'],
+                tasks: ['imagemin']
             }
         },
 
@@ -83,10 +97,11 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-remove-logging');
 
-    grunt.registerTask('default', ['concat', 'uglify', 'less', 'cssmin', 'removelogging', 'watch', 'jshint']);
+    grunt.registerTask('default', ['concat', 'uglify', 'less', 'cssmin', 'imagemin', 'removelogging', 'watch', 'jshint']);
     grunt.registerTask('debug', ['jshint']);
 
 };
